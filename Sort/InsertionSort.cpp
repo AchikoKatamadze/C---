fbedc51219ca2044ab416210f012afd82d06f078
Your Nameachikokatamadze7@gmail.comachikokatamadze7@gmail.comachikokatamadze7@gmail.comasf <you@example.com>
2023-12-28 11:35:11 +0400

    N2

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 using namespace std;
 
-void BubbleSort(vector<int>& arr, int x) {
+void InsertionSort(vector<int>& arr, int x) {
     if(x>1 && arr[x-1]<arr[x-2])
     {
         swap(arr[x-1], arr[x-2]);
-        BubbleSort(arr, x-1);
+        InsertionSort(arr, x-1);
     }
     if(arr.size()>x+1)
     {
         if(arr[x]>arr[x+1]) swap(arr[x], arr[x+1]);
-        BubbleSort(arr, x+1);
+        InsertionSort(arr, x+1);
     }
 }
 int main() {
@@ -25,7 +25,7 @@ int main() {
         cin >> x;
         arr[i] = x;
     }
-    BubbleSort(arr,0);
+    InsertionSort(arr,0);
     for (int i = 0; i < arr.size(); i++)
     {
         if (arr.size()-1 == i) cout << arr[i] << " ";
